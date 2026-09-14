93b0df2a6594e96682dcfa13587eadf7dcb798b5
codeshivamsi-sketch <code.shivam.si@gmail.com>
2026-09-14 13:17:14 +0530

    bug fix

--- api/app/db.py
+++ api/app/db.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 
 from app.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.database_url, echo=True)
 
 SessionFactory = async_sessionmaker(engine, expire_on_commit=False)
 
